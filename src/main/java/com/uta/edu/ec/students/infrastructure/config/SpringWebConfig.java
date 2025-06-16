9c4cc693c2d57b5a720ed9b367b142b6aa996b0e
Josue Joel Garcia Abata <josuegarcab2@hotmail.com>
2025-06-16 11:45:54 -0500

    add: cors

--- src/main/java/com/uta/edu/ec/students/infrastructure/config/SpringWebConfig.java
+++ src/main/java/com/uta/edu/ec/students/infrastructure/config/SpringWebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class SpringWebConfig {
@@ -11,6 +12,10 @@ public class SpringWebConfig {
     // Intercepta todas las peticiones HTTP POST y las convierte en peticiones HTTP PUT o DELETE
     @Bean
     public FilterRegistrationBean<HiddenHttpMethodFilter> hiddenHttpMethodFilter() {
-        return new FilterRegistrationBean<>(new HiddenHttpMethodFilter());
+        FilterRegistrationBean<HiddenHttpMethodFilter> registration = new FilterRegistrationBean<>(new HiddenHttpMethodFilter());
+        registration.addUrlPatterns("/*");
+
+        return registration;
     }
+
 }
